9a1ada7a3e9aa335681d67e6b821ec2661bca01b
Rafael Muñoz <r.munoz.software@gmail.com>
2019-03-08 09:00:14 +0100

    Added initial game data shape

---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,12 @@
+﻿namespace CrazySlots
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class Animal : MonoBehaviour
+    {
+
+    }
+}
+
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,26 @@
+﻿namespace CrazySlots
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class Game : MonoBehaviour
+    {
+        public Vector2 HeroPosition;
+        public Vector2 HeroTarget;
+        public float HeroSpeed;
+        public List<Animal> Followers;
+        public List<Animal> FreeAnimals;
+        public float SpeedIncreaseCooldownSeconds;
+        public int MaxFollowers = 5;
+        public int MaxAnimalsOnField = 10;
+        public int ScorePerAnimal = 1;
+        public float LevelDurationSeconds = 30f;
+        public int TotalScore = 0;
+        public float LevelTimeLeft = 0;
+        public float FakeDropDelaySeconds = 0.5f; 
+    }
+}
+
+
+
