85d20dd54654d59503f35d96cfa30397076db43a
Rafael Muñoz <r.munoz.software@gmail.com>
2019-03-08 11:26:40 +0100

    Added stream to trigger when player can gather animals

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -2,6 +2,7 @@
 {
     using System.Collections;
     using System.Collections.Generic;
+    using System.Linq;
     using UnityEngine;
     using UniRx;
     using UniRx.Triggers;
@@ -11,10 +12,11 @@
         private const float GroundZ = 0;
 
         public Transform Hero;
-        public ObservableCollision2DTrigger PenCollisionTrigger;
+        public ObservableTrigger2DTrigger PenCollisionTrigger;
         
         //public Vector2 HeroPosition;
         //public Vector2 HeroTarget;
+        public float HeroGatherRadious = 1f;
         public float HeroSpeed;
         public List<Animal> Followers;
         public List<Animal> FreeAnimals;
@@ -35,19 +37,35 @@
                     .Where ( collider => collider.gameObject == Hero.gameObject )
                     .Where ( _ => Followers.Count > 0 )
                     .Select( _ => Followers);
+                    
+            this.WhenHeroCanGatherAnimal = 
+                Observable.EveryUpdate()
+                    .Where( _ => Followers.Count < MaxFollowers)
+                    .Select( _ => FreeAnimals.FirstOrDefault( IsAnimalInGatherRange ))
+                    .Where ( animal => animal != null);
+
         }
 
         private void Update()
         {
             this.heroNavigator.Speed = HeroSpeed;
             this.heroNavigator.Update(Time.deltaTime);
-            
         }
 
         private void OnDrawGizmos()
         {
             if( Application.isPlaying )
+            {
                 Gizmos.DrawWireSphere(heroNavigator.Target, .1f);
+                Gizmos.DrawWireSphere(heroNavigator.CurrentPosition, HeroGatherRadious);
+            }
+        }
+
+        private bool IsAnimalInGatherRange(Animal animal)
+        {
+            Vector3 distanceVector = animal.transform.position - Hero.transform.position;
+            distanceVector.z = 0;
+            return distanceVector.magnitude < HeroGatherRadious;
         }
 
         private IObservable<Vector3> WhenUserClicks = 
@@ -59,8 +77,9 @@
                           groundPos.z = GroundZ;
                           return groundPos;
                       });
-        
+
         private IObservable<List<Animal>> WhenHeroDropsFollowersAtPen;
+        private IObservable<Animal> WhenHeroCanGatherAnimal;
         private Navigator heroNavigator;
     }
 }
