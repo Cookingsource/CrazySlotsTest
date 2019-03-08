106cfec5ef78efe6825d3abea6be4ce36afd2d1c
Rafael Muñoz <r.munoz.software@gmail.com>
2019-03-08 10:14:14 +0100

    Implemented basic navigator

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -3,11 +3,16 @@
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
+    using UniRx;
 
     public class Game : MonoBehaviour
     {
-        public Vector2 HeroPosition;
-        public Vector2 HeroTarget;
+        private const float GroundZ = 0;
+
+        public Transform Hero;
+        
+        //public Vector2 HeroPosition;
+        //public Vector2 HeroTarget;
         public float HeroSpeed;
         public List<Animal> Followers;
         public List<Animal> FreeAnimals;
@@ -19,6 +24,34 @@
         public int TotalScore = 0;
         public float LevelTimeLeft = 0;
         public float FakeDropDelaySeconds = 0.5f; 
+
+        private void OnEnable()
+        {
+            this.heroNavigator = new Navigator(HeroSpeed, Hero, WhenUserClicks);
+        }
+
+        private void Update()
+        {
+            this.heroNavigator.Speed = HeroSpeed;
+            this.heroNavigator.Update(Time.deltaTime);
+        }
+
+        private void OnDrawGizmos()
+        {
+            if( Application.isPlaying )
+                Gizmos.DrawWireSphere(heroNavigator.Target, .1f);
+        }
+
+        private IObservable<Vector3> WhenUserClicks = 
+            Observable.EveryUpdate()
+                      .Where( _ => Input.GetMouseButton(0))
+                      .Select( _ => 
+                      {
+                          Vector3 groundPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                          groundPos.z = GroundZ;
+                          return groundPos;
+                      });
+        private Navigator heroNavigator;
     }
 }
 
---
+++ Assets/Scripts/Navigator.cs
@@ -0,0 +1,40 @@
+﻿namespace CrazySlots
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UniRx;
+
+    public class Navigator 
+    {
+        private Transform mobile;
+        private Vector3 target;
+
+        public Navigator(float initialSpeed, Transform mobile, IObservable<Vector3> WhenTargetUpdates)
+        {
+            this.mobile = mobile;
+            this.Speed = initialSpeed;
+            WhenTargetUpdates.Subscribe( newTarget => target = newTarget);
+        }
+
+        public float Speed { get; set; }
+        public Vector3 Target { get { return target; } }
+        public Vector3 CurrentPosition { get { return mobile.position;}}
+
+        public void Update(float deltaTime)
+        {
+            Vector3 vectorToTarget = target - CurrentPosition;
+            float distanceUpdate = Speed * deltaTime;
+            float distanceToTarget = vectorToTarget.magnitude;
+            
+            if( distanceToTarget > distanceUpdate)
+            {
+                mobile.position += (vectorToTarget / distanceToTarget ) * distanceUpdate;
+            }
+            else
+                mobile.position = target;
+        }
+    }
+    
+}
+
